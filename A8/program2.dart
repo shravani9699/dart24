de3728c4da7a5b6accf806405a5efd90004b1285
shravani9699 <156292789+shravani9699@users.noreply.github.com>
2024-09-21 18:48:03 +0530

    Add files via upload

---
+++ A8/program2.dart
@@ -0,0 +1,14 @@
+import "dart:io";
+
+void main() {
+  print("Enter Number of rows:");
+  int rows = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < rows; j++) {
+      stdout.write("INC\t");
+    }
+
+    print("");
+  }
+}
